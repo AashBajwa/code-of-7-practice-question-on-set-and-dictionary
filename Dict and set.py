ad1d6a72db443224e60369cf45797f57c46b5167
AashBajwa <bajwayousaf303@gmail.com>
2026-09-21 17:29:04 +0500

    Add files via upload

---
+++ Dict and set.py
@@ -0,0 +1,91 @@
+#Practice question about dict and set
+#Store following word and meaning in a python dictionary
+'''dict={
+    "table" : ["a piece of furniture","list of fact and figure"],
+    "cat" : "a small animal"
+}
+print(dict)
+print(len(dict))
+print(type(dict))'''
+#You are given a list fo subject for students. Assume one classroom is required for 1 subject.How many classrooms are needed by all students.
+#"python","java","C++","python","javascript","java","python","java","C++","c"
+'''set={"python","java","C++","python","javascript","java","python","java","C++","c"}
+print(set)
+print(len(set))'''
+#Wap to enter marks of 3 subjects from the user and store them in a dictionary. Start with an empty dictionary & add one by one.use subject name as a key & marks as value.
+'''dict={}
+x=int(input("Enter phy marks: "))
+y=int(input("Enter che marks:  "))
+z=int(input("Enter Bio marks:  "))
+dict.update({"phy":x})
+dict.update({"che":y})
+dict.update({"Bio":z})
+print(dict)'''
+#WAP PRINT values of 9 and 9.0 in a single set
+'''set={9,9.0}
+print(set)'''#in such situation set concede always that these are same values so we can print with different method
+'''set={"9",9.0}
+print(set)'''#make one value as a string
+'''set={
+    ("int",9),
+    ("float",9.0)
+}
+print(set)'''#we store values in the form of tuple can;t in dictionary bcz dict is muteable but set is immutable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''null_dict={}
+print(null_dict)
+print(type(null_dict))
+tup=(),
+print(type(tup))
+list=[]
+print(type(list))
+str="Dream"
+print(type(str))
+set={1,2,3,"Ali",4+7j}
+print(type(set))'''
+#Wap in which print empty dict, enpty list ,set ,string and tuple
+'''d={}
+print(type(d))
+t=()
+print(type(t))
+s=""
+print(type(s))
+L=[]
+print(type(L))
+set=set()
+print(type(set))'''
+
